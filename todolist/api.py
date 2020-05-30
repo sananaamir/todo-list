d7cfa85bc03b2afce169cd68cbcb3111da5b132c
Sanan Aamir <sananaamir@gmail.com>
2020-05-29 23:28:27 -0500

    minor refactor

--- todolist/api.py
+++ todolist/api.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from todolist import app
 
-@app.route('/test')
-def test():
-    return render_template("index.html")
+@app.route('/')
+def index():
+    return {
+        "message": "Hello, dude!"
+    }
--- todolist/views.py
+++ todolist/views.py
@@ -1,8 +1,6 @@
 from flask import render_template
 from todolist import app
 
-@app.route('/')
-def index():
-    return {
-        "message": "Hello, dude!"
-    }
+@app.route('/test')
+def test():
+    return render_template("index.html")
